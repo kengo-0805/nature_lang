741a6e7b080fcca1339acd26a7cccedcdfabbc88
Kengo Horii <kengo0805ooo@gmail.com>
2019-12-29 12:44:44 +0900

    Initial Commit

---
+++ nature_lang/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  nature_lang
+//
+//  Created by 堀井健吾 on 2019/12/29.
+//  Copyright © 2019 kengohorii. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
