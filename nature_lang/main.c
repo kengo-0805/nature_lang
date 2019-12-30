af087752e8a7fc51599b6b568fa9dc55cb7f6e35
Kengo Horii <kengo0805ooo@gmail.com>
2019-12-30 22:57:35 +0900

    行程メモ

--- nature_lang/main.c
+++ nature_lang/main.c
@@ -11,5 +11,10 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("自然言語処理!\n");
+    //テキストファイル読み込み
+    //配列に入れる
+    //scanfで読み込み
+    //switchでカウント
+    //sortで並び替え
     return 0;
 }
